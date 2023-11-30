33b08e57cb234c87cdd422c2f53444fc95bf970d
deniyudi <deniyudi2@gmail.com>
2023-11-30 08:04:03 +0700

    koi

--- config/Database.js
+++ config/Database.js
@@ -3,16 +3,16 @@ import { Sequelize } from "sequelize";
 
 // Via Docker adminer 
 
-const db = new Sequelize('dppcompany','root','dpp',{
-    host:'db',
-    dialect:'mysql'
-})
+// const db = new Sequelize('dppcompany','root','dpp',{
+//     host:'db',
+//     dialect:'mysql'
+// })
 
 // Via Localhost XAMPP
 
-// const db = new Sequelize('testdpp','root','',{
-//     host:'localhost',
-//     dialect:'mysql'
-// })
+const db = new Sequelize('testdpp','root','',{
+    host:'localhost',
+    dialect:'mysql'
+})
 
 export default db;
